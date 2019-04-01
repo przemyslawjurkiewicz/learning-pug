d61dcedfc40612e5561f29e654f7d7f9a523d697
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-04-01 10:02:33 +0200

    Fix server.js

--- server.js
+++ server.js
@@ -2,17 +2,26 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'pug');
-app.set('views','./views');
+app.set('views', './views');
 
 app.use(express.static(__dirname + '/views'));
 
-app.get('/auth/google', function(req, res) {
-  res.render('google-auth.pug')
+app.get('/', function (req, res) {
+  res.render('index.pug')
 });
 
+app.get('/userform', function (req, res) {
+  res.render('userelogin.pug', {
+    name: req.query.first_name,
+    lastName: req.query.last_name
+  })
+})
 
+app.get('/auth/google', function (req, res) {
+  res.render('googlelogin.pug')
+})
 
-var server = app.listen(3000, 'localhost', function() {
+var server = app.listen(3000, 'localhost', function () {
   var host = server.address().address;
   var port = server.address().port;
 
